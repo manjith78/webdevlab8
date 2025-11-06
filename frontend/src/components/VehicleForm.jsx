6452950c5dffc7509ca5ced80ba7ec931e7442cd
manjith78 <manjithmanjith78@gmail.com>
2025-11-06 16:11:07 +0530

    Added build script for Vercel

--- frontend/src/components/VehicleForm.jsx
+++ frontend/src/components/VehicleForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/vehicles";
+const API_URL = "https://webdevlab8.onrender.com";
 
 export default function VehicleForm() {
   const [form, setForm] = useState({
